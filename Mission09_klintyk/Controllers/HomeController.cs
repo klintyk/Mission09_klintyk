0b299d036d188c053013f69aba4c3969e0614f25
Kyle Klintworth <klintyk@byu.edu>
2023-03-01 23:05:18 -0700

    Add project files.

---
+++ Mission09_klintyk/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_klintyk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_klintyk.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+        //public HomeController(BookstoreContext temp)
+        //{
+        //    context = temp;
+        //}
+
+        //shortcut of above code
+        public HomeController(BookstoreContext temp) => context = temp;
+        public IActionResult Index()
+        {
+            var book = context.Books.ToList();
+            return View(book);
+        }
+    }
+}
