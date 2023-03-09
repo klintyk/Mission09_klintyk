18b5b31a8bb90e8c8d482f28ce1e39daaf1c4da7
Kyle Klintworth <klintyk@byu.edu>
2023-03-09 12:16:29 -0700

    all

--- Mission09_klintyk/Pages/Checkout.cshtml.cs
+++ Mission09_klintyk/Pages/Checkout.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission09_klintyk.Infrastructuer;
 using Mission09_klintyk.Models;
 
 namespace Mission09_klintyk.Pages
@@ -18,17 +19,20 @@ namespace Mission09_klintyk.Pages
         }
 
         public Cart cart { get; set; }
-        public void OnGet(Cart c)
+        public string ReturnUrl { get; set; }
+        public void OnGet(string returnurl)
         {
-            cart = c;
+            ReturnUrl = returnurl ?? "/";
+            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
-        public IActionResult OnPost(int bookid)
+        public IActionResult OnPost(int bookid, string returnUrl)
         {
             Book b = repo.Books.FirstOrDefault(x => x.BookId == bookid);
             cart = new Cart();
             cart.AddItem(b, 1);
-            return RedirectToPage(cart);
+            HttpContext.Session.SetJson("cart", cart);
+            return RedirectToPage(new { ReturnUrl = returnUrl });
         }
     }
 }
